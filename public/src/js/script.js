d845d108f03fcf29c027b478658254f9c7542896
HanWoong Kim <hanationbear@gmail.com>
2016-09-07 11:03:43 +0900

    navigation active, hover 효과 추가, ScollMagic 연결

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -2,7 +2,8 @@
   'use strict';
 
   var $html = $('html'),
-  $nav = $('.navigation'),
+  $nav_home = $('.nav-home'),
+  $nav_list = $('.nav-list'),
   $scroll_nav = $('.scroll-nav');
   
   // js 설정 가는 환경 체크 
@@ -11,13 +12,19 @@
     changeDeviceMode($html, event.data.i++ === 0 ? true : false);
   });
 
-  // page scroll
-  $nav.find('a').on('click', function () {
-    // console.log(this);
+  // page navigation
+
+  $nav_home.find('a').on('click', function () {
     gotoPages.call(this, event);
   });
 
-  // scroll navigation
+  $nav_list.find('a').on('click', function () {
+    gotoPages.call(this, event);
+    var $this = $.$(this);
+    $this.parent().radioClass('active');
+  });
+
+  // side scroll navigation
   $scroll_nav.find('a').on('click', function () {
     gotoPages.call(this, event);
     var $this = $.$(this);
@@ -36,6 +43,30 @@
     .setClassToggle('.scroll-nav', 'fade-in')
     .addTo(ctrl);
 
+  var page_list_scene = '.nav-page01, .nav-page02, .nav-page03, .nav-page04, .nav-page05'.split(', ');
+  page_list_scene.forEach(function (selector, idx) {
+    var scroll_scene = new ScrollMagic.Scene({
+          'triggerElement': `.page0${idx+1}`,
+          'triggerHook': 0.5,
+          'duration': $(`.page0${idx+1}`).height()
+          // 'reverse': false
+      })
+      .setClassToggle(selector, 'active')
+      .addTo(ctrl);
+  });
+
+  var nav_list_scene = '.nav-list01, .nav-list02, .nav-list03, .nav-list04, .nav-list05'.split(', ');
+  nav_list_scene.forEach(function (selector, idx) {
+    var scroll_scene = new ScrollMagic.Scene({
+          'triggerElement': `.page0${idx+1}`,
+          'triggerHook': 0.5,
+          'duration': $(`.page0${idx+1}`).height()
+          // 'reverse': false
+      })
+      .setClassToggle(selector, 'active')
+      .addTo(ctrl);
+  });
+
   // var home_pin = new ScrollMagic.Scene({
   //   'triggerElement': `.home`,
   //   'triggerHook': 0,
@@ -50,19 +81,6 @@
   // $('.navigation .nav-home').on('click', function () {
   //   TweenMax.from(this, 1, {css: {rotation: 180, opacity:0, scale:0.5}, ease: Quad.easeInOut});
   // })
-
-  var scene_list = '.nav-list01, .nav-list02, .nav-list03, .nav-list04, .nav-list05'.split(', ');
-
-  scene_list.forEach(function (selector, idx) {
-    var scroll_scene = new ScrollMagic.Scene({
-          'triggerElement': `.page0${idx+1}`,
-          'triggerHook': 0.5,
-          'duration': $(`.page0${idx+1}`).height()
-          // 'reverse': false
-      })
-      .setClassToggle(selector, 'active')
-      .addTo(ctrl);
-  });
 
 
 })(this, this.jQuery);
