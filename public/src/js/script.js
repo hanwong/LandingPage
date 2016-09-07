6045d891fa7e45ea09d257b2c4b5edcfcd3e94da
HanWoong Kim <hanationbear@gmail.com>
2016-09-07 11:44:42 +0900

    script.js 요소 영역 별로 코드 정리

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -12,8 +12,7 @@
     changeDeviceMode($html, event.data.i++ === 0 ? true : false);
   });
 
-  // page navigation
-
+  // gnb navigation 링크 이동시 active 클래스 추가
   $gnb_home.find('a').on('click', function () {
     gotoPages.call(this, event);
   });
@@ -24,25 +23,10 @@
     $this.parent().radioClass('active');
   });
 
-  // side scroll navigation
-  $side_nav.find('a').on('click', function () {
-    gotoPages.call(this, event);
-    var $this = $.$(this);
-    $this.parent().radioClass('active');
-  });
-
+  // gnb navigation 영역 ScrollMagic 추가
   var ctrl = new ScrollMagic.Controller({
     'addIndicators': true
   });
-
-  var scroll_scene = new ScrollMagic.Scene({
-        'triggerElement': '.page01',
-        'triggerHook': 0.3,
-        // 'reverse': false
-    })
-    .setClassToggle('.side-nav', 'fade-in')
-    .addTo(ctrl);
-
   var gnb_scene = '.gnb-page01, .gnb-page02, .gnb-page03, .gnb-page04, .gnb-page05'.split(', ');
   gnb_scene.forEach(function (selector, idx) {
     var scroll_scene = new ScrollMagic.Scene({
@@ -55,6 +39,22 @@
       .addTo(ctrl);
   });
 
+  // side navigation 링크 이동시 active 클래스 추가
+  $side_nav.find('a').on('click', function () {
+    gotoPages.call(this, event);
+    var $this = $.$(this);
+    $this.parent().radioClass('active');
+  });
+
+  var scroll_scene = new ScrollMagic.Scene({
+        'triggerElement': '.page01',
+        'triggerHook': 0.3,
+        // 'reverse': false
+    })
+    .setClassToggle('.side-nav', 'fade-in')
+    .addTo(ctrl);
+
+  // side navigation 영역 ScrollMagic 추가
   var side_scene = '.side-page01, .side-page02, .side-page03, .side-page04, .side-page05'.split(', ');
   side_scene.forEach(function (selector, idx) {
     var scroll_scene = new ScrollMagic.Scene({
